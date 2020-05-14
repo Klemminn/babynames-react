cdfa81d2228818a67f401e11395dae49dbcfe281
klemenzhrafn <klemenz.hrafn.kristjansson@curron.is>
2020-05-14 16:29:39 +0000

    Remove unused staging environment

--- src/environment/index.tsx
+++ src/environment/index.tsx
@@ -5,25 +5,13 @@ type EnvParams = {
 const production: EnvParams = {
   api: 'https://api.nafnaleit.com/v1'
 }
-const staging: EnvParams = {
-  api: 'https://stagingapi.nafnaleit.com/v1'
-}
 
 const local: EnvParams = {
   api: 'http://127.0.0.1:8000/v1'
-  // api: 'https://api.nafnaleit.com/v1'
 }
 
-let envParams: EnvParams
+const env = process.env.REACT_APP_ENV
 
-const env = process.env.REACT_APP_ENV || 'local'
-
-if (env === 'production') {
-  envParams = production
-} else if (env === 'staging') {
-  envParams = staging
-} else {
-  envParams = local
-}
+const envParams: EnvParams = env === 'production' ? production : local
 
 export default envParams
