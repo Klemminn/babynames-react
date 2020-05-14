b86d3d81a0d364cc8ce42f97fd71a7f3ad0ef999
klemminn <klemenz88@gmail.com>
2020-05-14 00:37:10 +0000

    Add environment parameters to compiles

--- src/environment/index.tsx
+++ src/environment/index.tsx
@@ -2,21 +2,21 @@ type envParams = {
   api: string
 }
 
-const production = {
+const production: envParams = {
   api: 'http://api.nafnaleit.com/v1'
 }
-const staging = {
+const staging: envParams = {
   api: 'https://stagingapi.nafnaleit.com/v1'
 }
 
-const local = {
+const local: envParams = {
   // api: 'http://api.nafnaleit.com/v1'
   api: 'http://127.0.0.1:8000/v1'
 }
 
 let envParams: envParams
 
-const env = String(process.env)
+const env = process.env.REACT_APP_ENV || 'local'
 
 if (env === 'production') {
   envParams = production
